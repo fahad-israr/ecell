6d949bb9911cd16e8f2e18421368a8006e1273a8
fahad00cms <fahad00cms@gmail.com>
2019-01-19 02:28:53 +0530

    Updates

---
+++ precache-manifest.2209d998d42b58f7e6145db2c69e00a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "fahad00cms.github.io/ecell/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "2eb49487b895231ced016ef4b0da4ffa",
+    "url": "fahad00cms.github.io/ecell/static/media/earthmap.2eb49487.jpg"
+  },
+  {
+    "revision": "79e8fa64a8503259d3ff45d06fbc01b8",
+    "url": "fahad00cms.github.io/ecell/static/media/clouds.79e8fa64.png"
+  },
+  {
+    "revision": "d8c77d57ef1ee4d09880",
+    "url": "fahad00cms.github.io/ecell/static/js/runtime~main.d8c77d57.js"
+  },
+  {
+    "revision": "9d8fe7005b203e0bc9eb",
+    "url": "fahad00cms.github.io/ecell/static/js/main.9d8fe700.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "fahad00cms.github.io/ecell/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "9d8fe7005b203e0bc9eb",
+    "url": "fahad00cms.github.io/ecell/static/css/main.036e6de3.chunk.css"
+  },
+  {
+    "revision": "8bc16933884ed4ce54a7ac9327e74f76",
+    "url": "fahad00cms.github.io/ecell/index.html"
+  }
+];
